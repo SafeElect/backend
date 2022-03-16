7c02b630f5a11d3726733c3609bb73abf9a281fb
myprgit <mehmetyapar@windowslive.com>
2022-03-16 11:44:17 +0300

    sql file update

--- safeelect.sql
+++ safeelect.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2022 at 05:56 AM
+-- Generation Time: Mar 16, 2022 at 09:43 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `safeelect`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `candidate`
+--
+
+CREATE TABLE `candidate` (
+  `id` int(11) NOT NULL,
+  `nid` bigint(20) NOT NULL,
+  `first` varchar(255) NOT NULL,
+  `last` varchar(255) NOT NULL,
+  `voteCount` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `candidate`
+--
+
+INSERT INTO `candidate` (`id`, `nid`, `first`, `last`, `voteCount`) VALUES
+(1, 11111111111, 'MEHMET', 'YAPAR', 1),
+(2, 11111111113, 'BEYZA', 'YILMAZ', 0);
 
 -- --------------------------------------------------------
 
@@ -70,11 +92,20 @@ CREATE TABLE `voter` (
 --
 
 INSERT INTO `voter` (`id`, `nid`, `first`, `last`, `mother_first`, `father_first`, `address`, `gender`, `bday`, `bcity`, `pass`) VALUES
-(0, 11111111111, 'MEHMET', 'YAPAR', 'AYŞE', 'ALİ', 'ÇANKAYA', 'Male', '1993-02-11', 'ANKARA', '$2b$10$ets2arucjoLI8a2ecZMnvew1eozrZXdCRL8NckjiO5fsUyqShMOLq');
+(1, 11111111111, 'MEHMET', 'YAPAR', 'AYŞE', 'ALİ', 'ÇANKAYA', 'Male', '1993-02-10', 'ANKARA', '$2b$10$ets2arucjoLI8a2ecZMnvew1eozrZXdCRL8NckjiO5fsUyqShMOLq'),
+(2, 11111111112, 'ALİ', 'VELİ', 'ZEHRA', 'MUSTAFA', 'KEÇİÖREN', 'Male', '2002-10-09', 'ANKARA', '$2b$10$rZXfRI5pLjAwG9o8AtUE.eaD8smfQrZGIGkGmXqpHdxKece//7bsu'),
+(4, 11111111113, 'BEYZA', 'YILMAZ', 'ZEYNEP', 'EMİN', 'YENİMAHALLE', 'Female', '1998-03-27', 'İSTANBUL', '$2b$10$7EDXorHjL3ZBX.N2eMDrkO8N/FYSRxfHTaGz6CVi.vYPRMspn5noK');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `candidate`
+--
+ALTER TABLE `candidate`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uid` (`nid`);
 
 --
 -- Indexes for table `stats`
@@ -92,6 +123,12 @@ ALTER TABLE `voter`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `candidate`
+--
+ALTER TABLE `candidate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `stats`
