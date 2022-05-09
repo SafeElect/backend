e53cbc2b089c2eb52e95376a3a66bf16374d86d6
myprgit <mehmetyapar@windowslive.com>
2022-05-10 01:00:35 +0300

    pushed for mali

--- routes/isvoting.js
+++ routes/isvoting.js
@@ -6,10 +6,12 @@ const router = express.Router();
 let votingState = 1;
 router.get('/isvoting', (_, res) => {
     sendResponse(res, 200, votingState);
+    
 });
 router.get('/isvoting/end', (_, res) => {
     votingState = 0;
     sendResponse(res, 200, "voting ended");
+    console.log(votingState);
 });
 
 module.exports = router;
