f0e414d16084077c77c8ccc9b1f8140c07c88f1b
myprgit <mehmetyapar@windowslive.com>
2022-05-10 01:57:17 +0300

    changes for panda

--- routes/smob.js
+++ routes/smob.js
@@ -33,18 +33,59 @@ router.get('/smob/:votedfor', (req, res) => {
         let sum = male + female;
         let malep = male/sum*100;
         let femalep = female/sum*100;
-        let ageSum = 0;
+        //18-30 31-50 51-60 61+
+        let et = 0;
+        let tf = 0;
+        let fs = 0;
+        let sp = 0;
         returningValues.forEach(e => {
-            ageSum += e.age;
+            if(e.age < 30 && e.age > 18)
+            {
+              et++;
+            }
+            else if(e.age < 50 && e.age > 31)
+            {
+              tf++;
+            }
+            else if(e.age < 60 && e.age > 51)
+            {
+              fs++;
+            }
+            else
+            {
+              sp++;
+            }
         });
-        let avgAge = ageSum/returningValues.length;
+        // let avgAge = ageSum/returningValues.length;
         let precentages = {male: malep, female: femalep};
         let uniqueCities = [...new Set( returningValues.map(obj => obj.bcity)) ];
         let cities={};
         uniqueCities.forEach(el => {
             cities[el]=(returningValues.filter(value => value.bcity == el).length/returningValues.length*100);
         });
-        sendResponse(res, 200, {data: returningValues,genderPercentages: precentages, avgAge: avgAge, cityPercentages: cities});
+        let cityKey = Object.keys(cities);
+        let cityVal = Object.values(cities);
+
+        if(cityKey.length < 3)
+        {
+          if(!cityKey.includes("ANKARA"))
+          {
+            cityKey.push("ANKARA")
+            cityVal.push(0);
+          }
+          if(!cityKey.includes("İSTANBUL"))
+          {
+            cityKey.push("İSTANBUL")
+            cityVal.push(0);
+          }
+          if(!cityKey.includes("İZMİR"))
+          {
+            cityKey.push("İZMİR")
+            cityVal.push(0);
+          }
+        }
+
+        sendResponse(res, 200, {data: returningValues,genderPercentages: malep, age: [et,tf,fs,sp], location: cityKey, locationPercentage: cityVal});
       }
     });
   });
